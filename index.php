f278108eba98cb4bd6fd938274aa401b6140419a
chris-coraggio <ccoraggi@purdue.edu>
2016-11-28 16:51:46 -0500

    removed form file

--- index.php
+++ index.php
@@ -7,15 +7,15 @@
 	<div id="content">
 		<div id="container">
 			<h2>Tip Calculator</h2>
-			<form method="post" action="form.php">
+			<form method="post" action="index.php">
 				Bill subtotal: $
-				<input id="subtotal" type="text">
+				<input id="subtotal" name="subtotal" type="text">
 				<br>
 				Tip percentage:
 				<br>
 <?php
 for ($percent = 10; $percent < 25; $percent += 5) {
-	echo "<input id=\"{$percent}_percent\" type=\"radio\" name=\"percentage\" value=\"{$percent}%\">{$percent}%";
+	echo "<input id=\"{$percent}_percent\" type=\"radio\" name=\"percentage\" value=\".{$percent}\">{$percent}%";
 }
 ?>
 				<br>
@@ -23,4 +23,15 @@ for ($percent = 10; $percent < 25; $percent += 5) {
 			</form>
 		</div>
 	</div>
+
+	<?php
+function showTip() {
+	$total = (1 + (double) $_POST["percentage"]) * $_POST["subtotal"];
+	$tip = ($total - $_POST["subtotal"]);
+	echo "<div class=\"results\"> <br>Tip: " . $tip . "<br>Total: " . $total;
+}
+if (isset($_POST)) {
+	showTip();
+}
+?>
 </body>
