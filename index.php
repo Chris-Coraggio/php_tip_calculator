4749f9bbc317d00679fe63b434f36b01f635aa8f
chris-coraggio <ccoraggi@purdue.edu>
2016-11-30 23:54:32 -0500

    should be fully functional

--- index.php
+++ index.php
@@ -15,11 +15,14 @@
 				<br>
 <?php
 
+$custom_checked = ' checked="checked"';
+
 for ($percent = 10; $percent < 25; $percent += 5) {
 
 	$checked = '';
-	if ((isset($_POST["percentage"]) && $_POST["percentage"] == $percent / 100)) {
+	if ((isValid($_POST["percentage"]) && $_POST["percentage"] == $percent / 100)) {
 		$checked = ' checked="checked"';
+		$custom_checked = "";
 	}
 
 	echo "<input id=\"{$percent}_percent\" type=\"radio\" name=\"percentage\" value=\".{$percent}\"" .
@@ -27,19 +30,54 @@ for ($percent = 10; $percent < 25; $percent += 5) {
 }
 ?>
 				<br>
+				<input id="custom_percent" type="radio"
+				name="percentage" value="custom" <?php if (isValid($_POST["percentage"])) {
+	echo $custom_checked;
+}
+?>>Custom: <input type="text" name="custom_percent" value="<?php echo isValid($_POST["custom_percent"]) ? $_POST["custom_percent"] : '' ?>"> %
+				<br>
+				Split: <input type="text" name="split_num" value="<?php echo isValid($_POST["split_num"]) ? $_POST["split_num"] : "1" ?>"> person(s)
+				<br>
 				<input id="submit_button" type="submit">
 			</form>
 			<div id="results">
 				<?php
 if ("POST" === $_SERVER["REQUEST_METHOD"]) {
-	if ($_POST["subtotal"] <= 0 || !is_numeric($_POST["subtotal"])) {
-		echo "<div class=\"results\"> <br> Invalid subtotal";
-	} else if (!isset($_POST["percentage"])) {
-		echo "<div class=\"results\"> <br> Please select a tip percentage";
+	if (!isValid($_POST["subtotal"], true, 0.01)) {
+		display("Invalid subtotal");
+	} else if (!isValid($_POST["percentage"])) {
+		display("Please select a tip percentage");
 	} else {
+		if ($_POST["percentage"] == "custom") {
+			if (!isValid($_POST["custom_percent"], true)) {
+				display("Invalid custom percentage");
+			} else {
+				$_POST["percentage"] = $_POST["custom_percent"] / 100;
+			}
+		}
+
 		$total = (1 + (double) $_POST["percentage"]) * $_POST["subtotal"];
 		$tip = ($total - $_POST["subtotal"]);
-		echo "<div class=\"results\"> <br>Tip: $" . sprintf("%0.2f", $tip) . "<br>Total: $" . sprintf("%0.2f", $total);
+
+		if (isValid($_POST["split_num"], true, 1.01) && $_POST["split_num"] % 1 == 0) {
+			display("Tip: " . sprintf("$%0.2f", $tip) . "<br>Total: " . sprintf("$%0.2f", $total) . "<br>Tip each: " . sprintf("$%0.2f", ($tip / $_POST["split_num"])) . "<br>Total each: " . sprintf("$%0.2f", ($total / $_POST["split_num"])));
+		} else if (isValid($_POST["split_num"], true, 0.99) && $_POST["split_num"] % 1 == 0) {
+			display("Tip: " . sprintf("$%0.2f", $tip) . "<br>Total: " . sprintf("$%0.2f", $total));
+		} else {
+			display("Please enter a valid number of people");
+		}
+	}
+}
+
+function display($message) {
+	echo "<div class=\"results\"> <br> $message";
+}
+
+function isValid(&$param, $numeric = false, $min = 0) {
+	if ($numeric) {
+		return (isset($param) && is_numeric($param) && $param >= $min);
+	} else {
+		return isset($param);
 	}
 }
 ?>
